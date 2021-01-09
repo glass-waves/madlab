ee1c750e5709ce6b1b9ba5a90ca0ca41fec36b7b
Julianne Vela <cjvela@outlook.com>
2021-01-09 12:11:06 -0800

    Set layout of completed Mad-lib and entry form.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
+const userNoun1 = document.getElementById(noun - 1);
+const noun1 = document.getElementById(user - noun - 1);
+const createMadLib = document.getElementById(submit - button);
 
 // initialize state
 
 // set event listeners to update state and DOM
+
